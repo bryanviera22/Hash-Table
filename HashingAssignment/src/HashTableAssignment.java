1530b24c49921b10811654379124b2b88bad961c
Bryan Viera <72482373+bryanviera22@users.noreply.github.com>
2022-02-23 15:24:04 -0500

    Add files via upload

--- HashingAssignment/src/HashTableAssignment.java
+++ HashingAssignment/src/HashTableAssignment.java
@@ -2,8 +2,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class HashTableAssignment {
+
+    int collisions = 0;
+
+    String [] table = new String[12000];
 
     public HashTableAssignment() throws IOException {
 
@@ -12,7 +17,9 @@ public class HashTableAssignment {
 
         while(buffReader.ready()){
             String currentWord = buffReader.readLine();
-            System.out.println(currentWord + " hashes to " + hash(currentWord));
+           // System.out.println(currentWord + " hashes to " + hash(currentWord));
+
+            store(currentWord);
 
 //            System.out.println(hash(buffReader.readLine()));
         }
@@ -20,22 +27,50 @@ public class HashTableAssignment {
     }
 
     //hash converts each string into a number
-        public int hash(String s) {
+    public int hash(String word) {
 
         int num = 0;
 
-        System.out.println(s.length());
+        //System.out.println(s.length());
 
-            for(int i= 0; i<s.length(); i++){
-                num = num + (int)s.charAt(i);
-
-                System.out.println(num);
-
+            for(int i= 0; i<word.length(); i++){
+                num = num * (int)word.charAt(i);
             }
 
-            return num % 12000;
+          //  System.out.println(num%12000);
 
+            return num;
         }
+
+        //Store String in Index
+    public void store(String word) throws IOException {
+
+            if (table[hash(word)] == null) {
+                table[hash(word)] = word;
+            } else {
+                collisions++;
+                int index = hash(word);
+                int i = index;
+                while(i < table.length){
+                    if(table[i] == null){
+                        table[i] = word;
+                    }
+                    else{
+                        i++;
+                        collisions++;
+                    }
+                }
+            }
+
+        System.out.println(collisions);
+
+//            System.out.println(hash(word));
+//            System.out.println(word);
+        
+
+    }
+
+
 
 
 
